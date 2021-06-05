48739fa0491ecdbe1a52a04a49700633618b538e
Lais Arena <laisarena@gmail.com>
2021-06-05 14:21:04 -0300

    Correct case with no parameters

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: lfrasson <lfrasson@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/18 23:42:17 by lfrasson          #+#    #+#             */
-/*   Updated: 2021/06/03 11:47:22 by lfrasson         ###   ########.fr       */
+/*   Updated: 2021/06/05 11:09:33 by lfrasson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,13 +27,12 @@ int	ft_is_already_sort(t_list *list)
 	list = list->next;
 	while (list)
 	{
-		next_number =  (int *)list->content;
+		next_number = (int *)list->content;
 		if (*number < *next_number)
 			return (FALSE);
 		number = next_number;
 		list = list->next;
 	}
-
 	ft_lstclear(&list, ft_free_null);
 	return (TRUE);
 }
@@ -42,7 +41,7 @@ void	ft_initialize_stack_a(t_stack *stack_a, t_list *reverse_input)
 {
 	int	*number;
 
-	while(reverse_input)
+	while (reverse_input)
 	{
 		number = reverse_input->content;
 		ft_stack_push(stack_a, ft_new_stack_node(*number));
@@ -59,13 +58,12 @@ int	main(int argc, char **argv)
 	ft_new_stack(&stack_a);
 	reverse_input = NULL;
 	sort = NULL;
-
 	ft_parse(argc, argv, &reverse_input, &sort);
-	if(ft_is_already_sort(reverse_input))
+	if (ft_is_already_sort(reverse_input))
 		return (SUCCESS);
 	ft_initialize_stack_a(&stack_a, reverse_input);
 	ft_lstclear(&reverse_input, ft_free_null);
 	ft_sort(&stack_a, sort);
 	free(sort);
-	return(SUCCESS);
+	return (SUCCESS);
 }
