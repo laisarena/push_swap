31cdbe57089aaa4d9f503b2f155bf105117a00e9
Lais Arena <laisarena@gmail.com>
2021-06-05 17:32:47 -0300

    Version with swap when pushed to b

--- srcs/sort/sort_all.c
+++ srcs/sort/sort_all.c
@@ -6,12 +6,58 @@
 /*   By: lfrasson <lfrasson@student.42sp.org.b      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/05 15:00:20 by lfrasson          #+#    #+#             */
-/*   Updated: 2021/06/05 16:48:30 by lfrasson         ###   ########.fr       */
+/*   Updated: 2021/06/05 17:23:52 by lfrasson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "sort.h"
 
+static int	ft_can_swap_a(t_stack *stack_a)
+{
+	t_node	*top;
+	t_node	*second;
+
+	if (stack_a->size < 2)
+		return (FALSE);
+	top = stack_a->top;
+	second = stack_a->top->next;
+	if (top->group != second->group)
+		return (FALSE);
+	if (top->element > second->element)
+		return (TRUE);
+	return (FALSE);
+}
+
+static int	ft_can_swap_b(t_stack *stack_b)
+{
+	t_node	*top;
+	t_node	*second;
+
+	if (stack_b->size < 2)
+		return (FALSE);
+	top = stack_b->top;
+	second = stack_b->top->next;
+	if (top->group != second->group)
+		return (FALSE);
+	if (top->element < second->element)
+		return (TRUE);
+	return (FALSE);
+}
+
+void	ft_can_swap(t_stack *stack_a, t_stack *stack_b)
+{
+	int	sa;
+	int	sb;
+
+	sa = ft_can_swap_a(stack_a);
+	sb = ft_can_swap_b(stack_b);
+	if (sa && sb)
+		ft_double_swap(stack_a, stack_b);
+	//if (sa && !sb)
+	//	ft_swap_a(stack_a);
+	if (!sa && sb)
+		ft_swap_b(stack_b);
+}
 static void	ft_split_stack(t_stack *stack_a, t_stack *stack_b, t_pivot pivot)
 {
 	int	qtd;
