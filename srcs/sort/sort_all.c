dd20d4d5c02510a7897de06eec8b5290428ae90e
Lais Arena <laisarena@gmail.com>
2021-06-09 11:37:10 -0300

    Improve performance with splite size in stack b

--- srcs/sort/sort_all.c
+++ srcs/sort/sort_all.c
@@ -6,7 +6,7 @@
 /*   By: lfrasson <lfrasson@student.42sp.org.b      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/05 15:00:20 by lfrasson          #+#    #+#             */
-/*   Updated: 2021/06/06 16:23:54 by lfrasson         ###   ########.fr       */
+/*   Updated: 2021/06/10 21:17:48 by lfrasson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -93,20 +93,30 @@ static void	ft_split_stack_b(t_stack *stack_a, t_stack *stack_b, t_pivot pivot)
 		else
 			ft_rotate_b(stack_b);
 	}
-	
-	//while (stack_a->top->group == pivot.group)
-	while (stack_a->top->group == pivot.index)
-		ft_push_b(stack_a, stack_b);
 }
 
 static t_pivot	ft_get_pivot_b(int *sort, int size, int first)
 {
 	t_pivot	 pivot;
-	pivot.index = size / 2;
+
+	if (size < 10)
+		pivot.index = size - size / 2;
+	else if (size < 50)
+		pivot.index = size - size / 3;
+	else if (size < 90)
+		pivot.index = size - size / 5;
+	else if (size < 110)
+		pivot.index = size - size / 7;
+	else if (size < 150)
+		pivot.index = size - size / 9;
+	else
+		pivot.index = size - size / 11;
+	//pivot.index = size / 2;
+	//pivot.index = 2 * size / 3;
 	if (pivot.index > size - 2)
 		pivot.index = size - 2;
 	pivot.value = sort[pivot.index];
-	pivot.qtd = pivot.index;
+	pivot.qtd = size - pivot.index;
 	pivot.first = first;
 	return (pivot);
 }
@@ -114,7 +124,7 @@ static t_pivot	ft_get_pivot_b(int *sort, int size, int first)
 static t_pivot	ft_get_pivot(int *sort, int size, int first)
 {
 	t_pivot	 pivot;
-/*
+	
 	if (size < 10)
 		pivot.index = size / 2;
 	else if (size < 50)
@@ -127,7 +137,9 @@ static t_pivot	ft_get_pivot(int *sort, int size, int first)
 		pivot.index = size / 9;
 	else
 		pivot.index = size / 11;
-	*/pivot.index = size / 2;
+	//pivot.index = size / 2;
+	if (first)
+		pivot.index = 2 * size / 3;
 	if (pivot.index > size - 3)
 		pivot.index = size - 3;
 	pivot.value = sort[pivot.index];
@@ -135,21 +147,30 @@ static t_pivot	ft_get_pivot(int *sort, int size, int first)
 	pivot.first = first;
 	return (pivot);
 }
+
+void	ft_x(t_stack *stack_a, t_stack *stack_b, int *sort, int first)
+{
+	t_pivot	pivot;
+
+	if (!(first && stack_b->size > 3))
+		return ;
+	pivot = ft_get_pivot_b(sort, stack_b->size, first);
+	ft_split_stack_b(stack_a, stack_b, pivot);
+	ft_x(stack_a, stack_b, sort, first);
+	while (stack_a->top->group == pivot.index)
+		ft_push_b(stack_a, stack_b);
+}
+
 void	ft_sort_all(t_stack *stack_a, t_stack *stack_b, int *sort, int first)
 {
 	t_pivot	pivot;
-	t_pivot	pivot_b;
 
 	pivot.index = 0;
 	if (stack_a->size > 3)
 	{
 		pivot = ft_get_pivot(sort, stack_a->size, first);
 		ft_split_stack(stack_a, stack_b, pivot);
-		if (first && stack_b->size > 3)
-		{
-			pivot_b = ft_get_pivot_b(sort, pivot.qtd, first);
-			ft_split_stack_b(stack_a, stack_b, pivot_b);
-		}
+		ft_x(stack_a, stack_b, sort, first);
 		ft_sort_all(stack_a, stack_b, sort + pivot.index, 0);
 	}
 	ft_sort_three(stack_a, sort + pivot.index);
